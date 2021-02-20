943295319fa0e99de187d6bde6fd03046905ed3a
suz8a <suzbel97@gmail.com>
2021-02-19 22:00:50 -0700

    modal centered, added input to modal

--- src/modules/file-uploader/index.tsx
+++ src/modules/file-uploader/index.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import Modal from "@material-ui/core/Modal";
 import Card from "../../elements/card";
 import { ModalContainer } from "./styled";
+import { Grid, TextField } from "@material-ui/core";
+import Title from "../../elements/title";
+import { useState } from "react";
 
 type FileUploaderProps = {};
 
 function FileUploader() {
+  const [name, setName] = useState("");
+
+  function onChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    setName(e.target.value);
+  }
+
   return (
     <Modal
       open={true}
@@ -14,8 +25,22 @@ function FileUploader() {
       aria-describedby="simple-modal-description"
     >
       <ModalContainer>
-        <Card width="200px" height="200px">
-          <div>holi</div>
+        <Card width="800px" height="300px" padding="20px">
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Title text="Subir archivo" />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Nombre del archivo"
+                variant="outlined"
+                size="small"
+                onChange={(e) => onChange(e)}
+                value={name}
+                fullWidth
+              />
+            </Grid>
+          </Grid>
         </Card>
       </ModalContainer>
     </Modal>
