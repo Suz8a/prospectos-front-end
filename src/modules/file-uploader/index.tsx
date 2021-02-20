a7fca63bac2d6b5cf397a39f0e5f853b014fca3b
suz8a <suzbel97@gmail.com>
2021-02-19 23:38:11 -0700

    adding dropZone

--- src/modules/file-uploader/index.tsx
+++ src/modules/file-uploader/index.tsx
@@ -5,16 +5,23 @@ import { ModalContainer } from "./styled";
 import { Grid, TextField } from "@material-ui/core";
 import Title from "../../elements/title";
 import { useState } from "react";
+import { DropzoneDialog } from "material-ui-dropzone";
+import { DropzoneArea } from "material-ui-dropzone";
 
 type FileUploaderProps = {};
 
 function FileUploader() {
   const [name, setName] = useState("");
+  const [files, setFiles] = useState<File[]>([]);
 
   function onChange(
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) {
     setName(e.target.value);
+  }
+
+  function handleChange(files: File[]) {
+    setFiles(files);
   }
 
   return (
@@ -25,7 +32,7 @@ function FileUploader() {
       aria-describedby="simple-modal-description"
     >
       <ModalContainer>
-        <Card width="800px" height="300px" padding="20px">
+        <Card width="800px" height="auto" padding="20px">
           <Grid container spacing={3}>
             <Grid item xs={12}>
               <Title text="Subir archivo" />
@@ -38,6 +45,15 @@ function FileUploader() {
                 onChange={(e) => onChange(e)}
                 value={name}
                 fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <DropzoneArea
+                onChange={(files) => handleChange(files)}
+                showPreviews={true}
+                showPreviewsInDropzone={false}
+                useChipsForPreview
+                previewText="Selected files"
               />
             </Grid>
           </Grid>
