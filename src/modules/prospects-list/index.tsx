82640ad6b17bafbe656a19bd2c19ac6c66572f85
suz8a <suzbel97@gmail.com>
2021-02-21 20:32:30 -0700

    table shows content, added icon-text component

--- src/modules/prospects-list/index.tsx
+++ src/modules/prospects-list/index.tsx
@@ -4,9 +4,25 @@ import { useEffect } from "react";
 import { getPropspects } from "../../api";
 import TableInfo from "../../components/table-info";
 import Card from "../../elements/card";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 
 function ProspectsList() {
   const [prospects, setProspects] = useState([]);
+  const statusesTag = {
+    enviado: (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <CheckCircleIcon color="primary" fontSize="small" />
+        <div style={{ color: "rgb(63, 81, 181)" }}>enviado</div>
+      </div>
+    ),
+  };
+
   const columns = [
     { id: "nombre", label: "Nombre" },
     { id: "estatus", label: "Estatus" },
@@ -14,7 +30,7 @@ function ProspectsList() {
   var rows = prospects.map(
     ({ nombre, primerApellido, segundoApellido, estatus }) => ({
       nombre: `${nombre} ${primerApellido} ${segundoApellido}`,
-      estatus,
+      estatus: statusesTag[estatus],
     })
   );
 
